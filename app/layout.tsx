0da61652f8989959ef68ce5ac28354f4b7499aca
muhammadahmad338 <ahmadmuhammad.7700@gmail.com>
2025-11-16 15:24:36 +0500

    latest commits

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "DoraVPN - Fast & Secure VPN App",
+  title: "SoraVPN - Fast & Secure VPN App",
   description: "Fast & Secure VPN App",
 };
 
